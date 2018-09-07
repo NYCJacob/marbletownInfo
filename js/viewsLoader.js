19622aa7eb54a1320174673c4f0105d191ddaa4a
Jacob Sherman <jacob@jbsherman.com>
2018-09-06 20:52:19 -0400

    fix: census iframe reload in timeout block

--- js/viewsLoader.js
+++ js/viewsLoader.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $("#census").load("views/censusData.html");
+    // $("#census").load("views/censusData.html");
     $("#tboard").load("views/townBoard.html");
     $("#events").load("views/eventsLaw.html");
+
+    // reload embedded iframes
+    $("#census-tab").on("click", function () {
+        setTimeout(function () {
+            console.log("time out");
+            $('#cr-embed-06000US3611145458-demographics-age-distribution_by_decade-total').attr('src', function(i, val) {return val;});
+            $('#cr-embed-06000US3611145458-demographics-age-distribution_by_decade-total').attr('src', function(i, val) {return val;});
+            $('#cr-embed-06000US3611145458-demographics-age-distribution_by_category').attr('src', function(i, val) {return val;});
+            $('#cr-embed-06000US3611145458-demographics-sex').attr('src', function(i, val) {return val;});
+            $('#ccr-embed-06000US3611145458-demographics-race').attr('src', function(i, val) {return val;});
+            $('#cr-embed-06000US3611145458-economics-income-household_distribution').attr('src', function(i, val) {return val;});
+        }, 2000);
+    });
+
 });
