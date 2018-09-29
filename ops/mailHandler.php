14f36b663138fc99724ed8973f2a06353114652e
Jacob Sherman <jacob@jbsherman.com>
2018-09-28 20:37:35 -0400

    form working, needs server side validation

--- ops/mailHandler.php
+++ ops/mailHandler.php
@@ -18,10 +18,24 @@ use Mailgun\Mailgun;
 $mgClient = new Mailgun($mgApiKey);
 $domain = "mg.marbletowninfo.org";
 
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$street = $_POST['street'];
+$city = $_POST['city'];
+$zipcode = $_POST['zipcode'];
+$email = $_POST['e_mail'];
+$adult = $_POST['adult_confirm'];
+$acknowledge = $_POST['acknowledgment'];
+
+$messageText = "$first_name $last_name is over 18 [code $adult ] and signed by acknowledgement [code $acknowledge ] \n
+address: $street, $city  $zipcode \n
+email: $email";
+
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
     'from'    => 'Excited User <mailgun@mg.marbletowninfo.org>',
-    'to'      => 'Baz <jacob@jbsherman.com>',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
+    'to'      => 'MarbletownInfo <jacob@jbsherman.com>',
+    'subject' => 'Petition signature',
+    'text'    => $messageText
 ));
